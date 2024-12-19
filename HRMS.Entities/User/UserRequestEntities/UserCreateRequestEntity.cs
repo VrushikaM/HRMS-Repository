f05eab720b1c7c48de9a32ae8fe4056c25443158
VrushikaM <tbs.vrushika@gmail.com>
2024-12-19 11:07:32 +0530

    create validator for create the user using fluent validation

--- HRMS.Entities/User/UserRequestEntities/UserCreateRequestEntity.cs
+++ HRMS.Entities/User/UserRequestEntities/UserCreateRequestEntity.cs
@@ -2,10 +2,10 @@
 {
     public class UserCreateRequestEntity
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public bool IsActive { get; set; }
     }
 }
