2d934b20a984b2adfd8f66df85cadbd6e6e65a15
Arzoo <arzoo@gmail.com>
2024-12-31 18:35:06 +0530

    initial commit

--- HRMS.Utility/AutoMapperProfiles/Tenant/TenantRegistrationMapping/TenantRegistrationMappingProfile.cs
+++ HRMS.Utility/AutoMapperProfiles/Tenant/TenantRegistrationMapping/TenantRegistrationMappingProfile.cs
@@ -1,8 +1,24 @@
 ﻿using AutoMapper;
+using HRMS.Dtos.Tenant.TenantRegistration.TenantRegistrationRequestDtos;
+using HRMS.Dtos.Tenant.TenantRegistration.TenantRegistrationResponseDtos;
+using HRMS.Entities.Tenant.TenantRegistration.TenantRegistrationRequestEntities;
+using HRMS.Entities.Tenant.TenantRegistration.TenantRegistrationResponseEntities;
 
 namespace HRMS.Utility.AutoMapperProfiles.Tenant.TenantRegistrationMapping
 {
     public class TenantRegistrationMappingProfile : Profile
     {
+        public TenantRegistrationMappingProfile()
+        {
+
+            CreateMap<TenantRegistrationCreateRequestDto, TenantRegistrationCreateRequestEntity>();
+
+
+            CreateMap<TenantRegistrationCreateRequestEntity, TenantRegistrationCreateResponseEntity>();
+
+
+            CreateMap<TenantRegistrationCreateResponseEntity, TenantRegistrationCreateResponseDto>();
+        }
+
     }
 }
