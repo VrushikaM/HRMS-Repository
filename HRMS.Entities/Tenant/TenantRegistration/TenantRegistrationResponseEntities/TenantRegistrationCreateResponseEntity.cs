1499ab313548ffa7a086350a95624d7dacda03db
Arzoo <arzoo@gmail.com>
2024-12-31 19:05:19 +0530

    initial commit

--- HRMS.Entities/Tenant/TenantRegistration/TenantRegistrationResponseEntities/TenantRegistrationCreateResponseEntity.cs
+++ HRMS.Entities/Tenant/TenantRegistration/TenantRegistrationResponseEntities/TenantRegistrationCreateResponseEntity.cs
@@ -2,6 +2,7 @@
 {
     public class TenantRegistrationCreateResponseEntity
     {
+        public int UserId { get; set; }
         public string SubdomainName { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
