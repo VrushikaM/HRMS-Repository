a7783e90f5cc0bc81adf9c08565f534990bb6992
VrushikaM <tbs.vrushika@gmail.com>
2025-01-01 16:09:53 +0530

    organization crud completed with all changes

--- HRMS.Dtos/Tenant/Organization/OrganizationResponseDtos/OrganizationUpdateResponseDto.cs
+++ HRMS.Dtos/Tenant/Organization/OrganizationResponseDtos/OrganizationUpdateResponseDto.cs
@@ -4,6 +4,8 @@
     {
         public int OrganizationID { get; set; }
         public string OrganizationName { get; set; } = string.Empty;
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
         public int UpdatedBy { get; set; }
         public DateTime UpdatedAt { get; set; }
         public bool IsActive { get; set; }
--- HRMS.Entities/Tenant/Organization/OrganizationResponseEntities/OrganizationReadResponseEntity.cs
+++ HRMS.Entities/Tenant/Organization/OrganizationResponseEntities/OrganizationReadResponseEntity.cs
@@ -5,10 +5,10 @@
         public int OrganizationID { get; set; }
         public string? OrganizationName { get; set; }
         public int CreatedBy { get; set; }
-        public int? UpdatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
         public bool IsActive { get; set; }
-        public bool IsDeleted { get; set; }
+        public bool IsDelete { get; set; }
     }
 }
