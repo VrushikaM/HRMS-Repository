bf96ff75604ad206591fb094495e7700ee1e342d
Arzoo <arzoo@gmail.com>
2025-01-03 14:51:43 +0530

    create tenant registration complete

--- HRMS.PersistenceLayer/Repositories/TenantRegistrationRepository.cs
+++ HRMS.PersistenceLayer/Repositories/TenantRegistrationRepository.cs
@@ -16,13 +16,15 @@ namespace HRMS.PersistenceLayer.Repositories
         {
             _dbConnection = dbConnection;
         }
-
         public async Task<TenantRegistrationCreateResponseEntity> CreateTenantRegistration(TenantRegistrationCreateRequestEntity tenantregistration)
         {
             var parameters = new DynamicParameters();
             parameters.Add("@UserId", dbType: DbType.Int32, direction: ParameterDirection.Output);
             parameters.Add("@SubdomainId", dbType: DbType.Int32, direction: ParameterDirection.Output);
-            parameters.Add("@TenantId ", dbType: DbType.Int64, direction: ParameterDirection.Output);
+            parameters.Add("@TenantId", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            parameters.Add("@OrganizationId", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            parameters.Add("@UserRoleId", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            parameters.Add("@DomainId", dbType: DbType.Int32, direction: ParameterDirection.Output);
             parameters.Add("@SubdomainName", tenantregistration.SubdomainName);
             parameters.Add("@FirstName", tenantregistration.FirstName);
             parameters.Add("@LastName", tenantregistration.LastName);
@@ -30,24 +32,32 @@ namespace HRMS.PersistenceLayer.Repositories
             parameters.Add("@Email", tenantregistration.Email);
             parameters.Add("@Password", tenantregistration.Password);
 
-            await _dbConnection.ExecuteAsync(TenantRegistrationStoreProcedures.CreateTenantRegistration, parameters, commandType: CommandType.StoredProcedure);
+            await _dbConnection.ExecuteAsync( TenantRegistrationStoreProcedures.CreateTenantRegistration,parameters,commandType: CommandType.StoredProcedure);
 
             var userId = parameters.Get<int>("@UserId");
             var subdomainId = parameters.Get<int>("@SubdomainId");
+            var tenantId = parameters.Get<int>("@TenantId");
+            var organizationId = parameters.Get<int>("@OrganizationId");
+            var domainId = parameters.Get<int>("@DomainId");
+            var userRoleId = parameters.Get<int>("@UserRoleId");
             var hashedPassword = PasswordHashingUtility.HashPassword(tenantregistration.Password);
+
             var createdTenantregistration = new TenantRegistrationCreateResponseEntity
             {
                 UserId = userId,
                 SubdomainId = subdomainId,
+                TenantId = tenantId,
+                OrganizationId = organizationId,
+                DomainId = domainId,
                 SubdomainName = tenantregistration.SubdomainName,
                 FirstName = tenantregistration.FirstName,
                 LastName = tenantregistration.LastName,
                 UserName = tenantregistration.UserName,
                 Email = tenantregistration.Email,
-                Password = hashedPassword
+                Password = hashedPassword,
+                UserRoleId = userRoleId,
             };
-
-            return createdTenantregistration;
+          return createdTenantregistration;
         }
     }
 }
